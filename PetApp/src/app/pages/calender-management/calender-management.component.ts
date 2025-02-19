5fac3841f425ad7b6a3279050817fc209e5280af
regine <regine.issan@icloud.com>
2025-02-19 21:31:18 +0200

    Added Angular Router with Sidebar Navigation without Layout component

---
+++ PetApp/src/app/pages/calender-management/calender-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calender-management',
+  templateUrl: './calender-management.component.html',
+  styleUrls: ['./calender-management.component.scss']
+})
+export class CalenderManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ PetApp/src/app/pages/pets-management/pets-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-management',
+  templateUrl: './pets-management.component.html',
+  styleUrls: ['./pets-management.component.scss']
+})
+export class PetsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
